9ab4aab81be3c6a499f86af2404027f72268d2d2
姚思源 <yaosiyuan@MryaodeMacBook-Pro.local>
2026-06-01 19:53:49 +0800

    Connect PayPal payment links

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 window.RADAR_CONFIG = {
-  checkoutUrl: "https://www.paypal.com/ncp/payment/YOUR_STARTER_LINK_ID",
-  starterCheckoutUrl: "https://www.paypal.com/ncp/payment/YOUR_STARTER_LINK_ID",
-  proCheckoutUrl: "https://www.paypal.com/ncp/payment/YOUR_PRO_LINK_ID",
-  sponsorCheckoutUrl: "https://www.paypal.com/ncp/payment/YOUR_SPONSOR_LINK_ID",
+  checkoutUrl: "https://www.paypal.com/ncp/payment/65RBUJ8G49YB4",
+  starterCheckoutUrl: "https://www.paypal.com/ncp/payment/65RBUJ8G49YB4",
+  proCheckoutUrl: "https://www.paypal.com/ncp/payment/AUC82VEBNWQSL",
+  sponsorCheckoutUrl: "https://www.paypal.com/ncp/payment/ASXXBHU643YG4",
   supportEmail: "support@example.com",
   starterPrice: "$4.99",
   proPrice: "$9.99",
